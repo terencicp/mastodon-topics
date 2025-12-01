e74c9e1ed0320e8c7451d93b279c5681fb1c928a
Terenci Claramunt <47529793+terencicp@users.noreply.github.com>
2025-12-01 16:39:06 +0100

    Added push.py

---
+++ pipeline/push.py
@@ -0,0 +1,18 @@
+# Push new data to GitHub
+
+import subprocess
+from pathlib import Path
+from utils import setup_logging_format, get_date_twodaysago
+
+logger = setup_logging_format()
+_, yesterday_ymd = get_date_twodaysago()
+repository = Path(__file__).parent.parent
+
+try:
+    subprocess.run(['git', 'add', '.'], cwd=repository, check=True)
+    subprocess.run(['git', 'commit', '-m', f'Added push.py'], cwd=repository, check=True)
+    subprocess.run(['git', 'push'], cwd=repository, check=True)
+    logger.info(f'Pushed data to GitHub.')
+    
+except subprocess.CalledProcessError as e:
+    logger.error(f'Git command failed: {e}')
